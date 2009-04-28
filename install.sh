cad26e1fe406036f9cfad47cd2df955f1dc88731
Hartmut Holzgraefe <hartmut@php.net>
2009-04-28 15:03:04 +0000

    install drizzle generator

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 (cd CodeGen_PECL/PECL/Tools/ && plex ProtoLexer.plex >/dev/null && phplemon ProtoParser.y )
 
-for a in CodeGen CodeGen_PECL CodeGen_MySQL CodeGen_MySQL_UDF CodeGen_MySQL_Plugin
+for a in CodeGen CodeGen_PECL CodeGen_MySQL CodeGen_MySQL_UDF CodeGen_MySQL_Plugin CodeGen_Drizzle
 do
 	echo $a
 	(cd $a; pear install --force --offline package2.xml)
